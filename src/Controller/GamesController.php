acb7eb3d2add3de20c6a42c197cacdf0f10e7b88
Baptiste Dombry <baptiste.dombry12@gmail.com>
2022-11-08 23:51:50 +0100

    Adding Games + first try to Games

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Games;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GamesController extends AbstractController
 {
-    #[Route('/games', name: 'app_games')]
-    public function index(): Response
+
+    public function showGamesByPlatform(string $platform, EntityManagerInterface $em): Response
     {
-        return $this->render('games/index.html.twig', [
-            'controller_name' => 'GamesController',
-        ]);
+        $repo = $em->getRepository(Games::class);
+        $games = $repo->findBy(['platform' => $platform]);
+        return $this->render('games/index.html.twig', ['games' => $games]);
+    }
+
+    public function showGameByPlatformAndId(string $platform, string $id): Response
+    {
+        return new Response("<h1>".$platform." + ".$id."</h1>");
     }
 }
